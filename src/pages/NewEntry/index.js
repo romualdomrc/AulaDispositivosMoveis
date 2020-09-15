d2a46da7d1bd9a38621b7491cfcc8158591ca5d8
Davi Queiroz <dvbarreira@hotmail.com>
2020-09-14 21:05:13 -0300

    Melhoria na identação e refatoracao do index da NewEntry

--- src/pages/NewEntry/index.js
+++ src/pages/NewEntry/index.js
@@ -1,11 +1,6 @@
 import React, {useState} from 'react'
 import {View,StatusBar, StyleSheet} from 'react-native'
-
-import ActionFooter, {
-  ActionPrimaryButton,
-  ActionSecondaryButton,
-} from '../../components/Core/ActionFooter'
-
+import ActionFooter, { ActionPrimaryButton, ActionSecondaryButton } from '../../components/Core/ActionFooter'
 import BalanceLabel from '../../components/BalanceLabel'
 import NewEntryInput from './NewEntryInput'
 import NewEntryCategoryPicker from './NewEntryCategoryPicker'
@@ -19,121 +14,116 @@ import Service from '../../services/Service'
 
 const NewEntry = ({route, navigation}) => {
 
-const entry = {
-    id: null,
-    amount: 0,
-    entryAt: new Date(),
-    photo: null,
-    address: null,
-    latitude: null,
-    longitude: null,
-    category: {id: null, name: 'Selecione'}
-}
+	const entry = {
+		id: null,
+		amount: 0,
+		entryAt: new Date(),
+		photo: null,
+		address: null,
+		latitude: null,
+		longitude: null,
+		category: {id: null, name: 'Selecione'}
+	}
 
-  const [, saveEntry, deleteEntry] = useEntries()
+	const [, saveEntry, deleteEntry] = useEntries()
 
-  const [debit, setDebit] = useState(entry.amount <= 0)
-  const [amount, setAmount] = useState(entry.amount)
-  const [category, setCategory] = useState(entry.category)
-  const [entryAt, setEntryAt] = useState(entry.entryAt)
-  const [photo, setPhoto] = useState(entry.photo)
-  const [address, setAddress] = useState(entry.address)
-  const [latitude, setLatitude] = useState(entry.latitude)
-  const [longitude, setLongitude] = useState(entry.longitude)
+	const [debit, setDebit] = useState(entry.amount <= 0)
+	const [amount, setAmount] = useState(entry.amount)
+	const [category, setCategory] = useState(entry.category)
+	const [entryAt, setEntryAt] = useState(entry.entryAt)
+	const [photo, setPhoto] = useState(entry.photo)
+	const [address, setAddress] = useState(entry.address)
+	const [latitude, setLatitude] = useState(entry.latitude)
+	const [longitude, setLongitude] = useState(entry.longitude)
 
-  const isValid = () => {
-    if (parseFloat(amount) !== 0) {
-      return true
-    }
+	const isValid = () => {
+		return parseFloat(amount) !== 0
+	}
 
-    return false
-  }
+  	const onSave = () => {
+		const data = {
+		amount: parseFloat(amount),
+		category: category,
+		photo: photo,
+		address: address,
+		latitude: latitude,
+		longitude: longitude,
+		entryAt: entryAt,
+		}
 
-  const onSave = () => {
-    const data = {
-      amount: parseFloat(amount),
-      category: category,
-      photo: photo,
-      address: address,
-      latitude: latitude,
-      longitude: longitude,
-      entryAt: entryAt,
-    }
+		// console.log('NewEntry :: save ', data)
+		saveEntry(data, entry)
+		onClose()
+  	}
 
-    // console.log('NewEntry :: save ', data)
-    saveEntry(data, entry)
-    onClose()
-  }
+	const onDelete = () => {
+		deleteEntry(entry)
+		onClose()
+	}
 
-  const onDelete = () => {
-    deleteEntry(entry)
-    onClose()
-  }
+	const onClose = () => {
+		navigation.goBack()
+	}
 
-  const onClose = () => {
-    navigation.goBack()
-  }
+	return (
+		<View style={styles.container}>
+			<StatusBar barStyle="light-content" backgroundColor={Colors.background} />
+			<BalanceLabel />
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor={Colors.background} />
-      <BalanceLabel />
+			<View style={styles.formContainer}>
+				<NewEntryInput
+					value={amount}
+					onChangeValue={setAmount}
+					onChangeDebit={setDebit}
+				/>
+				<NewEntryCategoryPicker
+					debit={debit}
+					category={category}
+					onChangeCategory={setCategory}
+				/>
 
-      <View style={styles.formContainer}>
-        <NewEntryInput
-          value={amount}
-          onChangeValue={setAmount}
-          onChangeDebit={setDebit}
-        />
-        <NewEntryCategoryPicker
-          debit={debit}
-          category={category}
-          onChangeCategory={setCategory}
-        />
-
-        <View style={styles.formActionContainer}>
-          <NewEntryDatePicker value={entryAt} onChange={setEntryAt} />
-          <NewEntryCameraPicker photo={photo} onChangePhoto={setPhoto} />
-          <NewEntryAddressPicker
-            address={address}
-            onChange={({latitude, longitude, address}) => {
-              setLatitude(latitude)
-              setLongitude(longitude)
-              setAddress(address)
-            }}
-          />
-          <NewEntryDeleteAction entry={entry} onOkPress={onDelete} />
-        </View>
-      </View>
-
-      <ActionFooter>
-        <ActionPrimaryButton
-          title={entry.id ? 'Salvar' : 'Adicionar'}
-          onPress={() => {
-            isValid() && onSave()
-          }}
-        />
-        <ActionSecondaryButton title="Cancelar" onPress={onClose} />
-      </ActionFooter>
-    </View>
-  )
+				<View style={styles.formActionContainer}>
+				<NewEntryDatePicker value={entryAt} onChange={setEntryAt} />
+				<NewEntryCameraPicker photo={photo} onChangePhoto={setPhoto} />
+				<NewEntryAddressPicker
+					address={address}
+					onChange={({latitude, longitude, address}) => {
+						setLatitude(latitude)
+						setLongitude(longitude)
+						setAddress(address)
+					}}
+				/>
+				<NewEntryDeleteAction entry={entry} onOkPress={onDelete} />
+			</View>
+			</View>
+				<ActionFooter>
+					<ActionPrimaryButton
+						title={entry.id ? 'Salvar' : 'Adicionar'}
+						onPress={() => {
+							isValid() && onSave()
+						}}
+					/>
+					<ActionSecondaryButton title="Cancelar" onPress={onClose} />
+				</ActionFooter>
+			</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Colors.background,
-    padding: 10,
-  },
-  formContainer: {
-    flex: 1,
-    paddingVertical: 20,
-  },
-  formActionContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginVertical: 10,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: Colors.background,
+		padding: 10,
+	},
+	formContainer: {
+		flex: 1,
+		paddingVertical: 20,
+	},
+	formActionContainer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		marginVertical: 10,
+	}
 })
 
 export default NewEntry
